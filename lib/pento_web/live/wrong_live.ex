26dacd19bd70651905e58295e26a1b06159dba40
deankinyua <kinyuadean@gmail.com>
2024-04-26 16:31:32 +0300

    added files

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -8,11 +8,19 @@ defmodule PentoWeb.WrongLive do
       <%= @message %>
     </h2>
     <h2>
-      <%= for n <- 1..10 do %>
+      <%= for n <- 1..5 do %>
         <a href="#" phx-click="guess" phx-value-number={n}><%= n %></a>
       <% end %>
     </h2>
+    <h2>
+      It's <%= @time %>
+    </h2>
+    <button phx-click="restartGame">Restart</button>
     """
+  end
+
+  def time() do
+    DateTime.utc_now() |> to_string
   end
 
   def mount(_params, _session, socket) do
@@ -21,23 +29,49 @@ defmodule PentoWeb.WrongLive do
       assign(
         socket,
         score: 0,
-        message: "Guess a number."
+        message: "Guess a number.",
+        time: time()
       )
     }
   end
 
+  def handle_event("restartGame", _value, socket) do
+    {:noreply,
+     socket
+     |> redirect(to: ~p"/guess")}
+  end
+
   def handle_event("guess", %{"number" => guess} = data, socket) do
     IO.inspect(data)
-    message = "Your guess: #{guess}. Wrong. Guess again. "
-    score = socket.assigns.score - 1
+    guess = String.to_integer(guess)
+    number = Enum.random(1..5)
 
-    {
-      :noreply,
-      assign(
-        socket,
-        message: message,
-        score: score
-      )
-    }
+    if guess === number do
+      message = "Your guess: #{guess}. Won. Restart the Game."
+      score = socket.assigns.score + 1
+
+      {
+        :noreply,
+        assign(
+          socket,
+          message: message,
+          score: score,
+          time: time()
+        )
+      }
+    else
+      message = "Your guess: #{guess}. Wrong. Guess again. "
+      score = socket.assigns.score - 1
+
+      {
+        :noreply,
+        assign(
+          socket,
+          message: message,
+          score: score,
+          time: time()
+        )
+      }
+    end
   end
 end
