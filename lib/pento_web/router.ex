f56651cce9d86670db80af0e4851a57b7b550f6c
deankinyua <kinyuadean@gmail.com>
2024-05-08 16:44:11 +0300

    created the context and the core

--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -20,6 +20,13 @@ defmodule PentoWeb.Router do
     get "/", PageController, :home
     live "/live", PageLive, :index
     live "/guess", WrongLive
+
+    live "/products", ProductLive.Index, :index
+    live "/products/new", ProductLive.Index, :new
+    live "/products/:id/edit", ProductLive.Index, :edit
+
+    live "/products/:id", ProductLive.Show, :show
+    live "/products/:id/show/edit", ProductLive.Show, :edit
   end
 
   # Other scopes may use custom stacks.
